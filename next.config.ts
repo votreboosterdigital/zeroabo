2af87af4da24e64f393ecf4c91dd2ea01bb829b8
votreboosterdigital <bayonnesportsante@gmail.com>
2026-03-03 16:59:01 -0500

    feat: logos Clearbit + config images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["logo.clearbit.com"],
+  },
 };
 
 export default nextConfig;
